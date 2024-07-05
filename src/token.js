abd214d10cc31b64b40b698473fa191c21e65a9c
Leonardo Lodi <your_email@address>
2024-07-05 12:11:51 +0100

    return functions and answer the questions

--- src/token.js
+++ src/token.js
@@ -5,8 +5,8 @@ const jwt = require('jsonwebtoken');
  * 
  * Documentation: https://www.npmjs.com/package/jsonwebtoken#jwtsignpayload-secretorprivatekey-options-callback
  */
-function createToken() {
-    
+function createToken(payload, secretKey) {
+    return jwt.sign(payload, secretKey)
 }
 
 /**
@@ -14,8 +14,8 @@ function createToken() {
  * 
  * Documentation: https://www.npmjs.com/package/jsonwebtoken#token-expiration-exp-claim
  */
-function createTokenWithExpiry() {
-    
+function createTokenWithExpiry(payload, secretKey, expiryTime) {
+    return jwt.sign(payload, secretKey, { expiresIn: expiryTime })
 }
 
 /**
@@ -23,8 +23,12 @@ function createTokenWithExpiry() {
  * 
  * Documentation: https://www.npmjs.com/package/jsonwebtoken#jwtverifytoken-secretorpublickey-options-callback
  */
-function verifyToken() {
-
+function verifyToken(token, secretKey) {
+    try {
+        return jwt.verify(token, secretKey)
+    } catch (error) {
+        return false
+    }
 }
 
 module.exports = {
